3c5e5bafacb19d1a26711a2cddcae0464502990c
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2025-05-01 21:35:34 +0100

    basic classes created for question and answer

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,3 +2,13 @@
 
 # define AskRequest(BaseModel) with a `question: str`
 # optionally define AskResponse(BaseModel) with an `answer: str`
+
+from pydantic import BaseModel
+
+# this expects a JSON body consisting of a string called "question"
+class AskRequest(BaseModel):
+    question: str
+
+# expects a JSON body with a string called "answer"
+class AskResponse(BaseModel): 
+    answser: str
