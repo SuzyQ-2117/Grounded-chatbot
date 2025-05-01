953ab3af0f709b1861e8128749535ab37596a478
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2025-05-01 22:07:01 +0100

    env loader created

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,3 +3,10 @@
 # load .env using load_dotenv()
 # keeps keys and configs tidy and secure
 # this is essentially code that fetches secrets from the .env file so that the secrets & variables stay secret
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+WIKI_API_URL = os.getenv("WIKI_API_URL")
